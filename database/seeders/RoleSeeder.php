804857184c27aa31ba8ab195177d8f31ea957350
Luis Alberto Jaime B <39748335+luisJaimeB@users.noreply.github.com>
2023-12-01 12:02:34 -0500

    Feat: Roles and permissions are added.

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role_admin = Role::create(['name' => 'admin']);
+        $role_editor = Role::create(['name' => 'editor']);
+
+        $permission_create_role = Permission::create(['name' => 'create roles']);
+        $permission_read_role = Permission::create(['name' => 'read roles']);
+        $permission_update_role = Permission::create(['name' => 'update roles']);
+        $permission_delete_role = Permission::create(['name' => 'delete roles']);
+
+        $permissions_admin = [$permission_create_role, $permission_read_role, $permission_update_role, $permission_delete_role];
+
+        $role_admin->syncPermissions($permissions_admin);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'luisyi1998@gmail.com',
+            'password' => Hash::make('admin')
+        ]);
+        $admin->assignRole('admin');
+    }
+}
